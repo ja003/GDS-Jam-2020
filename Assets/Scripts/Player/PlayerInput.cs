578fd4f285871ca87934ce2f628099d638a4070c
WickyPayne <wickypayne@gmail.com>
2020-11-07 11:40:34 +0100

    Player Movement

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerInput : MonoBehaviour
+{
+	private PlayerMovement movement;
+
+	private void Awake()
+	{
+		movement = GetComponent<PlayerMovement>();
+	}
+
+	private void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		movement.Move(h, v);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 1.0f;
+
+	private Animator AN_Animator;
+	private Rigidbody2D RB_Body;
+
+	public bool FacingLeft = true;
+	public bool FacingUp = true;
+
+	private void Awake()
+	{
+		AN_Animator = GetComponent<Animator>();
+		RB_Body = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		AN_Animator.SetFloat("hSpeed", RB_Body.velocity.x);
+	}
+
+	public void Move(float pMoveH, float pMoveV)
+	{
+		RB_Body.velocity = new Vector2(pMoveH * speed, pMoveV * speed);
+
+		if (pMoveH < 0 && !FacingLeft)
+		{
+			FlipH();
+		}
+		else if (pMoveH > 0 && FacingLeft)
+		{
+			FlipH();
+		}
+
+		if (pMoveV < 0 && FacingUp)
+		{
+			//FlipV();
+		}
+	}
+
+	private void FlipH()
+	{
+		FacingLeft = !FacingLeft;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	private void FlipV()
+	{
+		FacingUp = !FacingUp;
+
+		Vector3 theScale = transform.localScale;
+		theScale.y *= -1;
+		transform.localScale = theScale;
+	}
+}
