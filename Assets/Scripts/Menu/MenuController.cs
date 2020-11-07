e5ad591f2df4fc81d7beeb7cbb0c5e76c6bff63a
Adam Jurík <ja004@seznam.cz>
2020-11-07 09:38:48 +0100

    Basic region select
    - region select
    - added menu buttons
    - created main scenes

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] Button btnStart;
+    [SerializeField] Button btnSettings;
+    [SerializeField] Button btnAbout;
+    [SerializeField] Button btnExit;
+
+
+    [SerializeField] MenuMap map;
+
+
+    private void Awake()
+    {
+        btnStart.onClick.AddListener(OnBtnStart);
+    }
+
+    private void OnBtnStart()
+    {
+        gameObject.SetActive(false);
+        map.gameObject.SetActive(true);
+    }
+}
