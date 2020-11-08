d72702319f9d1424951186bcce974bc48d739dbb
Adam Jurík <ja004@seznam.cz>
2020-11-08 14:58:07 +0100

    Time controller
    - time left
    - fix prefab instantiate
    - time controller
    - periodical spawn

---
+++ Assets/Scripts/Game/TimeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : GameBehaviour
+{
+	private float timeStart;
+
+	private void Awake()
+	{
+		timeStart = Time.time;
+	}
+
+	public float GetTimeLeft()
+	{
+		return timeStart + game.MapController.Region.Time - Time.time;
+	}
+
+	private void Update()
+	{
+		Debug.Log($"Time left = {GetTimeLeft()}");
+	}
+}
